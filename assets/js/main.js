315c4403ae5a3ed2c040b51511e8f223703a7221
luan <luansca@icloud.com>
2021-06-29 13:58:19 -0300

    Scroll, link interno e rolagem

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@
 
 const   navMenu = document.getElementById('nav-menu'),
         navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+        navClose = document.getElementById('nav-close'),
+        navActive = document.getElementById('active')
+
 
 /*============== ATIVAR MENU ==============*/
 
@@ -11,7 +13,9 @@ if(navToggle)
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
+    
 }
+
 
 if(navClose)
 {
@@ -20,12 +24,72 @@ if(navClose)
     })
 }
 
+
+const wrapperMenu = document.getElementById('nav-bloco'),
+bloco = document.getElementById('bloco')
+
+bloco.addEventListener('click', () => {
+    event.preventDefault();
+wrapperMenu.classList.add('show-menu')
+
+
+})
+
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link'),
+        border = document.querySelectorAll('.nav__item')
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
     
-    navMenu.classList.remove('show-menu')
+    const navMenu = document.getElementById('nav-menu')
+
+        navMenu.classList.remove('show-menu')
+        
+console.log(wrapperMenu.addEventListener('click', () =>{ wrapperMenu.classList.remove('show-menu') }))
+
+}   
+        
+window.onscroll = function () {
+    showArrow()
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+const menuItens = document.querySelectorAll('.nav__bloco a[href^="#"]')
+        
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+
+  
+
+    window.scroll({
+        top: to + 100
+        
+    });
+}
+
+function showArrow()
+{
+   const arrowTop = document.getElementById('top');
+
+   if(document.documentElement.scrollTop > 90)
+   {
+        arrowTop.style.display = "block";
+   } else {
+    arrowTop.style.display = "none";
+   }
+}
+
+function subirTopo() {
+    document.documentElement.scrollTop = 0;
+}
