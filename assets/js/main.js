8c9a1b82be6e76ccf3e00fbaff60a3a4aedb4b7b
Luan Araujo <luaanscar@gmail.com>
2021-07-15 15:24:30 -0300

    playsinline

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,22 @@
 /*============== SHOW MENU ==============*/
 
-const   navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close'),
-        navActive = document.getElementById('active')
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close'),
+    navActive = document.getElementById('active')
 
 
 /*============== ATIVAR MENU ==============*/
 
-if(navToggle)
-{
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
-    
+
 }
 
 
-if(navClose)
-{
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
@@ -26,32 +24,32 @@ if(navClose)
 
 
 const wrapperMenu = document.getElementById('nav-bloco'),
-bloco = document.getElementById('bloco')
+    bloco = document.getElementById('bloco')
 
 bloco.addEventListener('click', () => {
     event.preventDefault();
-wrapperMenu.classList.add('show-menu')
+    wrapperMenu.classList.add('show-menu')
 
 
 })
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link'),
-        border = document.querySelectorAll('.nav__item')
+    border = document.querySelectorAll('.nav__item')
 
-function linkAction(){
-    
+function linkAction() {
+
     const navMenu = document.getElementById('nav-menu')
 
-        navMenu.classList.remove('show-menu')
-        
-console.log(wrapperMenu.addEventListener('click', () =>{ wrapperMenu.classList.remove('show-menu') }))
+    navMenu.classList.remove('show-menu')
 
-}   
+    console.log(wrapperMenu.addEventListener('click', () => { wrapperMenu.classList.remove('show-menu') }))
+
+}
 
 
-/*==================== SCROLL ====================*/   
-window.onscroll = function () {
+/*==================== SCROLL ====================*/
+window.onscroll = function() {
     showArrow()
 }
 
@@ -60,7 +58,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 const menuItens = document.querySelectorAll('.nav__bloco a[href^="#"]')
-        
+
 
 menuItens.forEach(item => {
     item.addEventListener('click', scrollToIdOnClick);
@@ -72,26 +70,27 @@ function scrollToIdOnClick(event) {
     const id = element.getAttribute('href');
     const to = document.querySelector(id).offsetTop;
 
-  
+
 
     window.scroll({
         top: to + 100
-        
+
     });
 }
 
-function showArrow()
-{
-   const arrowTop = document.getElementById('top');
+function showArrow() {
+    const arrowTop = document.getElementById('top');
 
-   if(document.documentElement.scrollTop > 90)
-   {
+    if (document.documentElement.scrollTop > 90) {
         arrowTop.style.display = "block";
-   } else {
-    arrowTop.style.display = "none";
-   }
+    } else {
+        arrowTop.style.display = "none";
+    }
 }
 
 function subirTopo() {
     document.documentElement.scrollTop = 0;
 }
+
+
+document.getElementById('vid').play();
